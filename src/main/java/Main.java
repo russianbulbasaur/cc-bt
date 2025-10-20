e18b46afa905e284595d4a169a5aa6687808cccd
Ankit <thakurankit036@gmail.com>
2025-10-20 13:46:00 +0530

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,21 +5,8 @@ public class Main {
   private static final Gson gson = new Gson();
 
   public static void main(String[] args) throws Exception {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
-    
     String command = args[0];
     if("decode".equals(command)) {
-      //  Uncomment this block to pass the first stage
-      //  String bencodedValue = args[1];
-      //  String decoded;
-      //  try {
-      //    decoded = decodeBencode(bencodedValue);
-      //  } catch(RuntimeException e) {
-      //    System.out.println(e.getMessage());
-      //    return;
-      //  }
-      //  System.out.println(gson.toJson(decoded));
 
     } else {
       System.out.println("Unknown command: " + command);
