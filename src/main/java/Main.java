dbe003fbd79723b9fcc6e9ba14a8cd800cf415d5
Ankit <thakurankit036@gmail.com>
2025-10-20 13:47:13 +0530

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,15 @@ public class Main {
   public static void main(String[] args) throws Exception {
     String command = args[0];
     if("decode".equals(command)) {
-
+        String bencodedValue = args[1];
+        String decoded;
+        try {
+            decoded = decodeBencode(bencodedValue);
+        } catch(RuntimeException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        System.out.println(gson.toJson(decoded));
     } else {
       System.out.println("Unknown command: " + command);
     }
